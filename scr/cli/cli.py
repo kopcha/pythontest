aab67045b21abab5b9aab5e3f68f65a23d1d651f
Volodymyr Kopcha <volodymyr.kopcha@gmail.com>
2015-03-10 23:15:26 +0200

    Sorting products by level according to RC Release procedure.

--- scr/cli/cli.py
+++ scr/cli/cli.py
@@ -3,15 +3,26 @@ Created on Mar 3, 2014
 
 @author: xvok
 '''
-from libs.common import sayBye
+from libs.common import sort_dictionary_to_list
 def main():
-    print('Hello World')
-    print('Hello World')
-    print('Hello World')
-    print('Hello World')
-    sayBye()
-    sayBye()
-    sayBye()
+    products = {'AXE10507/229': 'R1A',
+                'APT21009/229': 'R1A',
+                'ANT24009/1': 'R1A',
+                'ANT24808/25': 'R1A',
+                'CRT240010/2': 'R1A',
+                'CNT2401111': 'R3A',
+                'CNT2412222': 'R2A',
+                'CAAZ1071234': 'R7A',
+                'CAAZ1074321': 'R6A',
+                'CAAZA1072222': 'R5A',
+                'CAAZA1073333': 'R7A',
+                'CAA2045555': 'R8A',
+                'CXA2526666': 'R1A',
+                '7/CXC1465555': 'R8A',
+                '4/CXC1465555': 'R8A'}
+    print('Dictionary before sorting: {}'.format(products))
+    sorted_dict = sort_dictionary_to_list(products)
+    print('Dictionary after sorting: {}'.format(sorted_dict))
 
 if __name__ == '__main__':
     main()
--- scr/libs/common.py
+++ scr/libs/common.py
@@ -3,5 +3,34 @@ Created on Mar 3, 2014
 
 @author: xvok
 '''
+import re
 def sayBye():
     print('Good Bye!')
+
+
+def sort_dictionary_to_list(input_dict):
+    result = []
+    for level_no in range(len(LEVEL_PRIORITY)):
+        print('Current product level number: {}'.format(level_no))
+        for prod_no, r_state in input_dict.items():
+            match = re.match('^\d*\/*([A-Z]{3,})\d+\/*', prod_no)
+            if match:
+                prod_abc_class = match.group(1)
+                print('Retrieved product ABC class for {} {}: {}'.
+                      format(prod_no, r_state, prod_abc_class))
+                if prod_abc_class in LEVEL_PRIORITY[level_no]:
+                    result.append((prod_no, r_state))
+                else:
+                    print('Product ABC class {} is not in the list!'.
+                          format(prod_abc_class))
+            else:
+                print('Can not retrieve ABC class for {} {}'.
+                      format(prod_no, r_state))
+    return result
+
+LEVEL_PRIORITY = {0: ['CAA', 'CXA', 'CXC', 'CAAZ', 'CAAZA'],
+                  1: ['CNT', 'CNTU'],
+                  2: ['CRT', 'CRTU'],
+                  3: ['ANT'],
+                  4: ['APT'],
+                  5: ['AXE']}
